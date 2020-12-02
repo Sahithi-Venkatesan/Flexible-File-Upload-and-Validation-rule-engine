91558e03f2179e1b1f0efc75ff7a57e4c5f9eb7d
Shreya <42346881+shreya2031@users.noreply.github.com>
2020-12-03 04:00:27 +0530

    Added code to retrieve files(in comments)

--- Validation-fields/main.py
+++ Validation-fields/main.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import json
 import pandas as pd
 from io import StringIO
 from pandas_schema import Column, Schema
 from pandas_schema.validation import LeadingWhitespaceValidation, TrailingWhitespaceValidation, CanConvertValidation, MatchesPatternValidation, InRangeValidation, InListValidation
-
+import os
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
+
+#code to retrieve uploaded file
+'''
+app.secret_key = "secret key"
+
+Ext_check = set(['csv', 'txt'])
+
+def file_check(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in Ext_check
+	
+
+@app.route('/', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+       
+		file = request.files['file']
+		if file.filename == '':
+			flash('Please select a file')
+			return render_template('inputfield.html')
+		if file and file_check(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(file.filename)  # stores file in main folder
+            #file.save(os.path.join('Validation-fields', filename))  -->to store file in Validation-fields
+			flash('File successfully uploaded')
+			return render_template('inputfield.html')
+		else:
+			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+			return render_template('inputfield.html')
+'''            
 
 @app.route('/', methods=['post', 'get'])
 def login():
@@ -28,6 +58,7 @@ def login():
         ])
 
         test_data = pd.read_csv('data.csv')
+        #test_data = pd.read_csv(file.filename)--> rename to consider the file that is being uploaded
 
         errors = schema.validate(test_data)
 
