1b6aab9240be7ef19139ff2f5fa06dd13f44017e
Sahithi Venkatesan <sahi.48.3@gmail.com>
2020-12-02 21:52:23 +0530

    Modified the boxes logic stuff

--- Validation-fields/main.py
+++ Validation-fields/main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import csv
 import json
 import pandas as pd
 from io import StringIO
@@ -9,15 +8,13 @@ from pandas_schema.validation import LeadingWhitespaceValidation, TrailingWhites
 app = Flask(__name__)
 
 @app.route('/', methods=['post', 'get'])
-
 def login():
     message = ''
     if request.method == 'POST':
         l1 = request.form.get('l1')  # access the data inside 
         l2 = request.form.get('l2')
         l3 = request.form.get('l3')
-        
-        '''
+
         x = {
             "col_name": l1,
             "dtype": l2,
@@ -25,25 +22,18 @@ def login():
             }
 
         print(x)
-        '''
-        
+
         schema = Schema([
         Column('l1', [LeadingWhitespaceValidation(), TrailingWhitespaceValidation()])
-        col_name=["l1"]
-        test_data = pd.read_csv("data.csv",header=None,error_bad_lines=False)
+        ])
+
+        test_data = pd.read_csv('data.csv')
 
         errors = schema.validate(test_data)
 
         for error in errors:
             print(error)
     return render_template('inputfield.html', message=message)
-'''
-def fields():
-    if request.method=='POST'
-        d1=request.form1.get('d1')
-        print(d1)
-    return render_template('inputfield.html')
-'''
 
 '''
         if (l1 == 'sahithi' and l2 == 'int' and l3 == '7'):
