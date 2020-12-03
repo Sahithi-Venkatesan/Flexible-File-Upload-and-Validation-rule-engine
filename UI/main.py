b12bc1bed55f2e9fe6c11c7ed27b68c49580f7fa
Sahithi Venkatesan <sahi.48.3@gmail.com>
2020-12-03 20:30:18 +0530

    Linked up with the login page

--- UI/main.py
+++ UI/main.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 @app.route('/')  
 def home():
     return render_template("index.html")
+
+@app.route('/login_1', methods=['GET', 'POST'])
+def login_func():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('login_1.html')
     
+
 if __name__ == "__main__":   
     app.run(debug=True)
